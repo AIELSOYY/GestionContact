37f82ec921595446b09ab3a2a05dc6d7b579e8c7
AIELSOYY <alexandre.herisse21@gmail.com>
2024-02-09 12:33:56 +0100

    first push

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="app_accueil", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        /*$nomsStudents=['Jérémie', 'Ousmane', 'Alexia', 'Chouaibou'];
+        $age =17;
+        $nom="Stéphane";*/
+        return $this->render('accueil/index.html.twig', [
+            /*'controller_name' => 'AccueilController',*/
+            /*'leNom' => $nom,
+            'age' => $age,
+            'lesNoms' => $nomsStudents*/
+        ]);
+    }
+}
---
+++ src/Controller/ContactController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="app_contact")
+     */
+    public function listeContact(): Response
+    {
+        $manager=$this->$this->getDoctrine()->getManager();
+        $repo = $manager->getRepository(Contact::class);
+        $contacts=$repo->findAll();
+        return $this->render('contact/listeContacts.html.twig', [
+            /*'controller_name' => 'ContactController',*/
+            'lesContacts' => $contacts
+        ]);
+    }
+}
