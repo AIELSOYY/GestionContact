ab97f3187d00ebed1c10d22e1ede02c15f5f3721
AIELSOYY <alexandre.herisse21@gmail.com>
2024-02-13 16:41:24 +0100

    added more stuff

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
+use App\Repository\ContactRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,16 +11,29 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="app_contact")
+     * @Route("/contact", name="contacts", methods={"GET"})
      */
-    public function listeContact(): Response
+    public function listeContact(ContactRepository $repo): Response
     {
-        $manager=$this->getDoctrine()->getManager();
-        $repo=$manager->getRepository(Contact::class);
+        /*$manager=$this->getDoctrine()->getManager();
+        $repo=$manager->getRepository(Contact::class);*/
         $contacts=$repo->findAll();
+        //dump($contacts);
         return $this->render('contact/listeContacts.html.twig', [
             /*'controller_name' => 'ContactController',*/
             'lesContacts'=>$contacts,
         ]);
     }
+
+    /**
+     * @Route("/contact/{id}", name="ficheContact", methods={"GET"}))
+     */
+    public function ficheContact($id, ContactRepository $repo): Response
+    {
+        $contact=$repo->find($id);
+        return $this->render('contact/ficheContact.html.twig', [
+            'leContact'=>$contact,
+        ]);
+    }
+
 }
