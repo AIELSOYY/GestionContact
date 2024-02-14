5533ec40e6e3c61b43554149c7875b2aef913402
AIELSOYY <alexandre.herisse21@gmail.com>
2024-02-15 00:21:58 +0100

    Added categories

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategorieController extends AbstractController
+{
+    /**
+     * @Route("/categorie", name="categories", methods={"GET"})
+     */
+    public function listeCategorie(CategorieRepository $repo): Response
+    {
+        $categories=$repo->findAll();
+        return $this->render('categorie/listeCategorie.html.twig', [
+            //'controller_name' => 'CategorieController',
+            'lesCategories'=>$categories,
+        ]);
+    }
+
+    /**
+     * @Route("/categorie/{id}", name="ficheCategorie", methods={"GET"}))
+     */
+    public function ficheCategorie($id, CategorieRepository $repo): Response
+    {
+        $categorie=$repo->find($id);
+        return $this->render('categorie/ficheCategorie.html.twig', [
+            'laCategorie'=>$categorie,
+        ]);
+    }
+}
